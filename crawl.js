e6c03a53f6789bc559f92f7d8b072501f9b69703
StanimalTheMan <sjc591@nyu.edu>
2023-06-02 15:25:45 -0400

    Write initial normalizeURL func, get initial tests to pass

--- crawl.js
+++ crawl.js
@@ -1,9 +1,16 @@
 function normalizeURL(urlString) {
   // console.log(urlString)
   // console.log(new URL(urlString))
+  const urlStringObj = new URL(urlString)
+  const normalizedURLString = urlStringObj.hostname + urlStringObj.pathname
+  const normalizedURLStringLen = normalizedURLString.length
+  if (normalizedURLString[normalizedURLStringLen - 1] == '/') {
+    return normalizedURLString.substring(0, normalizedURLStringLen - 1)
+  }
+  return normalizedURLString
 }
 
-// normalizeURL('https://wagslane.dev/path/')
+// console.log(normalizeURL('https://wagslane.dev/path/'))
 // normalizeURL('https://wagsLane.Dev/path')
 // normalizeURL('https://wagslane.dev/path')
 // normalizeURL('http://wagslane.dev/path')
