9b58ae354365b527c3be6e6bd92792194e58b217
StanimalTheMan <sjc591@nyu.edu>
2023-07-10 07:53:24 -0400

    Rudimentarily accept baseURL cliArgument to scrape eventually

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+function main() {
+  const cliArguments = process.argv.slice(2);
+  numCliArguments = cliArguments.length;
+  if (numCliArguments !== 1) {
+    console.log(
+      "Invalid # of inputs entered: Only enter a base URL such as 'http://www.mlb.com'"
+    );
+    process.exit();
+  }
+  console.log(
+    "The web crawler is starting to crawl at the baseURL you entered."
+  );
+}
+
+main();
